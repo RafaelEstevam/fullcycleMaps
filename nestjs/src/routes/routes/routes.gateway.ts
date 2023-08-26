4be45e8aed2612320436361a8fda7fc8581ad7b8
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-08-26 19:49:33 -0300

    redis

--- nestjs/src/routes/routes/routes.gateway.ts
+++ nestjs/src/routes/routes/routes.gateway.ts
@@ -1,5 +1,8 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { RoutesDriverService } from '../routes-driver/routes-driver.service';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
 
 @WebSocketGateway({
   cors: {
@@ -7,13 +10,18 @@ import { Socket } from 'socket.io';
   },
 })
 export class RoutesGateway {
+  // constructor(private routesDriverService: RoutesDriverService) {}
+  constructor(@InjectQueue('new-points') private newPointsQueue: Queue) {}
+
   @SubscribeMessage('new-points')
-  handleMessage(
+  async handleMessage(
     client: Socket,
     payload: { route_id: string; lat: number; lng: number },
-  ): string {
+  ) {
+    // await this.routesDriverService.createOrUpdate(payload);
     client.broadcast.emit('admin-new-point', payload);
     client.broadcast.emit(`new-point/${payload.route_id}`, payload);
+    await this.newPointsQueue.add(payload);
     console.log(payload);
     return 'Hello world!';
   }
